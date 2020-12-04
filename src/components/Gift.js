eff6e426bbd804170b760c53b960a6faf39795cf
Anthony Paradowski <aparadowski@alteryx.com>
2020-12-04 15:16:40 -0700

    Completed gift functionality

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import {Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
 
 
 class Gift extends Component {
@@ -15,13 +15,21 @@ class Gift extends Component {
       <div>
         <Form>
           <FormGroup>
-            <ControlLabel>Person</ControlLabel>
+            <FormLabel>Person</FormLabel>
             <FormControl
               className="input-person"
               onChange={event => this.setState({person: event.target.value})}
             />
           </FormGroup>
+          <FormGroup>
+            <FormLabel>Present</FormLabel>
+            <FormControl
+              className="input-present"
+              onChange={event => this.setState({present: event.target.value})}
+            />
+          </FormGroup>
         </Form>
+        <Button className="btn-remove" onClick={() => this.props.removeGift(this.props.gift.id)}> Remove Gift </Button>
       </div>
     )
   }
